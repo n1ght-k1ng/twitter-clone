41a194a2499ee2208fe5fad2100673587cf37d60
Nikhil Reddy <77714082+n1ght-k1ng@users.noreply.github.com>
2022-10-18 02:28:41 +0530

    added post feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,28 @@
 import { SparklesIcon } from "@heroicons/react/outline"
 import Input from "./Input.js"
+import Post from "./post.js"
 
 export default function Feed() {
+  const posts =[ {
+    id: "1",
+    name: "Nikhil Reddy",
+    username: "n1ghtk1ng",
+    userImg: "https://media-exp1.licdn.com/dms/image/C4D03AQHHtZhcNvfJyQ/profile-displayphoto-shrink_800_800/0/1629191669010?e=1670457600&v=beta&t=uV7iAF6p3tpOaDxXsBzb4hwylD4EMRg9PbnIXTb5DDc",
+    img: "https://media-exp1.licdn.com/dms/image/C4D03AQHHtZhcNvfJyQ/profile-displayphoto-shrink_800_800/0/1629191669010?e=1670457600&v=beta&t=uV7iAF6p3tpOaDxXsBzb4hwylD4EMRg9PbnIXTb5DDc",
+    text: " nice view 1",
+    timestamp:" 2hours ago"
+  },
+  {
+    id: "2",
+    name: "Nikhil Reddy",
+    username: "n1ghtk1ng",
+    userImg: "https://media-exp1.licdn.com/dms/image/C4D03AQHHtZhcNvfJyQ/profile-displayphoto-shrink_800_800/0/1629191669010?e=1670457600&v=beta&t=uV7iAF6p3tpOaDxXsBzb4hwylD4EMRg9PbnIXTb5DDc",
+    img: "https://media-exp1.licdn.com/dms/image/C4D03AQHHtZhcNvfJyQ/profile-displayphoto-shrink_800_800/0/1629191669010?e=1670457600&v=beta&t=uV7iAF6p3tpOaDxXsBzb4hwylD4EMRg9PbnIXTb5DDc",
+    text: " nice view 1",
+    timestamp:" 2hours ago"
+  } ]
+
+
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-go max-w-wl ">
         <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-gray-200">
@@ -11,6 +32,9 @@ export default function Feed() {
             </div> 
         </div>
         <Input/>
+        {posts.map((post) =>(
+          <Post key={post.id} post ={post}/>
+        ))}
         </div>
   )
 }
